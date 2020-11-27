4bad1ea9f19182e510afab74e437a716a0eb8841
Isapoff <73703520+Isapoff@users.noreply.github.com>
2020-11-27 18:22:18 +0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -47,3 +47,30 @@
 # masha == sasha
 
 
+# task 4
+
+# class Complex(object):
+#     def __init__(self, real, imaginary):
+#         self.real = real
+#         self.imaginary = imaginary
+
+#     def __add__(self, no):
+#         return Complex(self.real + no.real, self.imaginary + no.imaginary)
+
+#     def __sub__(self, no):
+#         return Complex(self.real - no.real, self.imaginary - no.imaginary)
+
+#     def __mul__(self, no):
+#         return Complex(self.real * no.real - self.imaginary * no.imaginary,
+#                        self.real * no.imaginary + self.imaginary * no.real)
+
+#     def __truediv__(self, no):
+#         factor = no.real ** 2 + no.imaginary ** 2
+#         return Complex((self.real * no.real + self.imaginary * no.imaginary) / factor,
+#                        (self.imaginary * no.real - self.real * no.imaginary) / factor)
+
+#     def mod(self):
+#         return Complex((self.real ** 2 + self.imaginary ** 2) ** (1 / 2), 0)
+
+
+
